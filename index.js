25e3aa3e34ce0b967378f1ba365f47f0cb278e43
Rafael Veiga <dsg.rafael@gmail.com>
2022-07-02 15:37:02 -0300

    first

---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const dayjs = require("dayjs");
+
+readDir("./receipts/unorganized");
+
+function readDir(dir) {
+  const files = fs.readdirSync(dir);
+  for (const file of files) {
+    const filePath = `${dir}/${file}`;
+    if (fs.lstatSync(filePath).isDirectory()) {
+      readDir(filePath);
+    } else {
+      fs.stat(filePath, (err, stats) => {
+        console.log(filePath);
+        console.log(stats);
+
+        const newName = `${dayjs(stats.mtime).format("YYYY-MM-DD")}-${file}`;
+
+        fs.copyFileSync(filePath, `./receipts/organized/${newName}`);
+      });
+    }
+  }
+}
